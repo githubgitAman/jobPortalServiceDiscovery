f7d49f31096a2a29d9d1709a8159ac6905be683f
githubgitAman <sharma.aman.a30@gmail.com>
2025-04-26 18:30:37 +0530

    Created Service Discovery

---
+++ src/main/java/dev/aman/jobportalservicediscovery/JobPortalServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package dev.aman.jobportalservicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalServiceDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobPortalServiceDiscoveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/aman/jobportalservicediscovery/JobPortalServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aman.jobportalservicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
