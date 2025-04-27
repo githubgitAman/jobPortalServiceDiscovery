fe6a36ef49ba257311572de2d62b01c4e2283f85
githubgitAman <sharma.aman.a30@gmail.com>
2025-04-27 19:32:18 +0530

    Registered Auth Service in Service Discovery

--- src/main/java/dev/aman/jobportalservicediscovery/JobPortalServiceDiscoveryApplication.java
+++ src/main/java/dev/aman/jobportalservicediscovery/JobPortalServiceDiscoveryApplication.java
@@ -2,8 +2,10 @@ package dev.aman.jobportalservicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class JobPortalServiceDiscoveryApplication {
 
     public static void main(String[] args) {
